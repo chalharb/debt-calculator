55a7a40254b2c280abeede375af3e2a9852063e4
Logan Harber <chalharb@gmail.com>
2021-06-07 19:18:07 -0500

    re-init project using cra redux-typescript

--- src/views/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Hello World!', () => {
-  render(<App />);
-  const h1 = screen.getByText(/Debt Calculator/i);
-  expect(h1).toBeInTheDocument();
-});
