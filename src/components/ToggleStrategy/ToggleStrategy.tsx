47b9c4e47b5c2b146db6f54be5fa57719aafd7ec
Logan Harber <chalharb@gmail.com>
2021-06-20 10:15:26 -0500

    Moved component styles to styled.tsx and cleaned up ToggleStrategy.tsx

--- src/components/ToggleStrategy/ToggleStrategy.tsx
+++ src/components/ToggleStrategy/ToggleStrategy.tsx
@@ -1,59 +1,10 @@
 import React from 'react';
-import { Strategy } from './types';
-
-import { 
-    createStyles,
-    Theme, 
-    withStyles, 
-} from '@material-ui/core/styles';
-
-import { 
-    Grid, 
-    Switch,
-    Typography, 
-} from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { StyledSwitch } from './styled';
 
 // Redux
 import { useAppDispatch } from '../../app/hooks';
 import { updateStrategy } from './toggleStrategySlice';
-
-const AntSwitch = withStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            width: 28,
-            height: 16,
-            padding: 0,
-            display: 'flex',
-        },
-        switchBase: {
-            padding: 2,
-            color: theme.palette.common.white,
-            '&$checked': {
-                transform: 'translateX(12px)',
-                color: theme.palette.common.white,
-                '& + $track': {
-                    opacity: 1,
-                    backgroundColor: theme.palette.secondary.main,
-                    borderColor: theme.palette.secondary.main,
-                },
-            },
-        },
-        thumb: {
-            width: 12,
-            height: 12,
-            boxShadow: 'none',
-        },
-        track: {
-            border: `1px solid ${theme.palette.grey[500]}`,
-            borderRadius: 16 / 2,
-            opacity: 1,
-            backgroundColor: theme.palette.primary.main,
-            borderColor: theme.palette.primary.main,
-        },
-        checked: {},
-    }),
-)(Switch);
 
 const ToggleStrategy = () => {
     const dispatch = useAppDispatch();
@@ -63,9 +14,9 @@ const ToggleStrategy = () => {
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setState({ ...state, [event.target.name]: event.target.checked });
-        let strategy: Strategy = state.toggleValue ? 'snowball' : 'avalanche';
-        dispatch(updateStrategy(strategy));
+        dispatch(updateStrategy(state.toggleValue ? 'snowball' : 'avalanche'));
     };
+    
     return (
         <Typography component="div">
             <Grid component="label" container alignItems="center" spacing={1}>
