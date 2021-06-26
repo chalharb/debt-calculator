7d6d44092fb5199e7299c36e74d4f1e85b409cb2
Logan Harber <chalharb@gmail.com>
2021-06-25 19:31:07 -0500

    Created DebtForm for new debts

--- src/components/DebtModal/DebtModal.tsx
+++ src/components/DebtModal/DebtModal.tsx
@@ -5,6 +5,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DebtForm from '../DebtForm/DebtForm';
 
 interface Props {
     isOpen?: boolean;
@@ -33,6 +34,7 @@ const DebtModal:React.FC<Props> = ({ isOpen = false }) => {
                     <DialogContentText>
                         Enter your information to create a new debt.
                     </DialogContentText>
+                    <DebtForm/>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
